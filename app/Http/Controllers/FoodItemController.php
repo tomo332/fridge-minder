cbb3ab97770a349652eb7746e413a82d136bb895
tomo332 <zhijixi4@gmail.com>
2023-09-06 16:39:47 +0900

    画像表示用コード追加

--- app/Http/Controllers/FoodItemController.php
+++ app/Http/Controllers/FoodItemController.php
@@ -8,7 +8,8 @@ use App\Models\FoodItem;
 class FoodItemController extends Controller
 {
     public function foodview(){
-       return view('food_item');
+        $posts=FoodItem::all();
+       return view('food_item',compact('posts'));
     }
 
     public function foodadd(){
@@ -17,22 +18,34 @@ class FoodItemController extends Controller
 
     public function store(Request $request){
 
-
-        $post = FoodItem::create([
-            'food_name' => $request->food_name,
-            'quantity' => $request->quantity,
-            'expiration_date' => $request->expiration_date,
-            'best_before_date' => $request->best_before_date,
-            'tag' => $request->tag,
-            'photo_url' => $request->photo_url,
-        ]);
-
-        $validated = $request->validate([
+        $request->validate([
             'food_name' => 'required|max:20',
-            'tag' => 'required|max:15'
+            'tag' => 'required|max:15',
+            'photo_url' => 'image|mimes:jpeg,png,jpg|max:2048'
         ]);
 
-        $post = Post::create($validated);
+        $fileName = time() . '.' . $request->photo_url->extension();
+        $request->photo_url->storeAs('public/images', $fileName);
+
+        $post = new FoodItem;
+        $post->food_name = $request->input('food_name');
+        $post->quantity = $request->input('quantity');
+        $post->expiration_date = $request->input('expiration_date');
+        $post->best_before_date = $request->input('best_before_date');
+        $post->tag = $request->input('tag');
+        $post->photo_url = $fileName;
+        $post->save();
+        // $post = FoodItem::create([
+        //     'food_name' => $request->food_name,
+        //     'quantity' => $request->quantity,
+        //     'expiration_date' => $request->expiration_date,
+        //     'best_before_date' => $request->best_before_date,
+        //     'tag' => $request->tag,
+        //     'photo_url' => $request->photo_url,
+        // ]);
+
+
+        //$post = FoodItem::create($validated);
         return back()->with('message','保存しました');//セッションにメッセージを一時保存
     }
 }
