ae3de021860ba614f55fbbdbf04eaf50f7d3e416
tomo332 <zhijixi4@gmail.com>
2023-09-12 14:59:47 +0900

    ナビゲーションメニューの食品追加を削除

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,9 +15,9 @@
                     <x-nav-link :href="route('fooditem')" :active="request()->routeIs('fooditem')">
                         食品一覧
                     </x-nav-link>
-                    <x-nav-link :href="route('foodadd')" :active="request()->routeIs('foodadd')">
+                    {{-- <x-nav-link :href="route('foodadd')" :active="request()->routeIs('foodadd')">
                         食品追加
-                    </x-nav-link>
+                    </x-nav-link> --}}
                 </div>
             </div>
 
@@ -73,9 +73,9 @@
             <x-responsive-nav-link :href="route('fooditem')" :active="request()->routeIs('fooditem')">
                 食品一覧
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('foodadd')" :active="request()->routeIs('foodadd')">
+            {{-- <x-responsive-nav-link :href="route('foodadd')" :active="request()->routeIs('foodadd')">
                 食品追加
-            </x-responsive-nav-link>
+            </x-responsive-nav-link> --}}
         </div>
 
         <!-- Responsive Settings Options -->
